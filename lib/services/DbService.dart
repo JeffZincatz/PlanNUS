1df701fe6a0678704031606b7a3a404cf8c40e87
Bernardus Krishna <krishnabernardus@gmail.com>
2021-05-26 23:20:52 +0800

    Added new services for the database.

--- lib/services/DbService.dart
+++ lib/services/DbService.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:plannus/models/Event.dart';
 
 class DbService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final User currentUser = FirebaseAuth.instance.currentUser;
+  final CollectionReference events =
+      FirebaseFirestore.instance.collection("users")
+          .doc(FirebaseAuth.instance.currentUser.uid).collection("events");
 
   Future<String> getUserProfilePic() async {
     try {
@@ -41,4 +45,35 @@ class DbService {
       return null;
     }
   }
+
+  Future addNewEvent(Event event) async {
+    return await events.add(event.toMap());
+  }
+
+  List<Event> _eventListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.docs.map((doc) {
+      return Event(
+        id: doc.id ?? 1,
+        completed: doc.get("completed") ?? false,
+        passed: doc.get("passed") ?? false,
+        category: doc.get("category") ?? '',
+        description: doc.get("description") ?? '',
+        startTime: DateTime.parse(doc.get("startTime").toDate().toString()) ?? DateTime.now(),
+        endTime: DateTime.parse(doc.get("endTime").toDate().toString()) ?? DateTime.now(),
+      );
+    }).toList();
+  }
+
+  Stream<List<Event>> get eventsStream {
+    return events.snapshots().map(_eventListFromSnapshot);
+  }
+
+  Future markCompleted(Event event) async {
+    DocumentReference temp = events.doc(event.id);
+    return await temp.update({
+      "passed": true,
+      "completed": true,
+    });
+  }
+
 }
