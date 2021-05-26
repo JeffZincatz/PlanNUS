98abbbaf0fb419ceba51c01bee5076fe2cd885b1
Bernardus Krishna <krishnabernardus@gmail.com>
2021-05-26 23:19:00 +0800

    added calendar and the lists.

--- lib/screens/home/Home.dart
+++ lib/screens/home/Home.dart
@@ -1,7 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:plannus/screens/home/NavBar.dart';
+import 'package:plannus/screens/home/UnfinishedEventList.dart';
 import 'package:plannus/services/DbService.dart';
 import 'package:plannus/util/PresetColors.dart';
+import 'package:plannus/screens/home/Calendar2.dart';
+import 'package:plannus/screens/home/EventEditingPage.dart';
+import 'package:provider/provider.dart';
+import 'package:plannus/models/Event.dart';
+import 'package:plannus/screens/home/MaybeFinishedEventList.dart';
+import 'package:plannus/screens/home/CompletedEventList.dart';
+
+import '../../util/PresetColors.dart';
 
 class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
@@ -11,52 +21,116 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  DateTime currentDate = DateTime.now();
+
+  void updateCurrentDate(DateTime newDateTime) {
+    currentDate = newDateTime;
+  }
+
   final DbService _db = new DbService();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: PresetColors.blueAccent,
-        title: Text(
-          "PlanNUS",
-          style: TextStyle(
-            fontFamily: "Lobster Two",
-            fontWeight: FontWeight.bold,
-            fontStyle: FontStyle.italic,
-            fontSize: 32,
-            shadows: [
-              Shadow(
-                color: Colors.black,
-                offset: Offset(3, 3),
-                blurRadius: 10,
+
+    bool today(DateTime currentDate) {
+      return (currentDate.year == DateTime.now().year
+          && currentDate.month == DateTime.now().month
+          && currentDate.day == DateTime.now().day);
+    }
+
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    // print(_db.unfinishedEventsStream.isEmpty);
+
+    return StreamProvider<List<Event>>.value(
+      initialData: [],
+      value: _db.eventsStream,
+      builder: (context, snapshot) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: PresetColors.blueAccent,
+            title: Text(
+              "PlanNUS",
+              style: TextStyle(
+                fontFamily: "Lobster Two",
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                fontSize: 32,
+                shadows: [
+                  Shadow(
+                    color: Colors.black,
+                    offset: Offset(3, 3),
+                    blurRadius: 10,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          body: ListView(
+            children: [
+              Calendar2(updateCurrentDate: updateCurrentDate),
+              Divider(
+                height: screenHeight / 20,
+                thickness: 3.0,
+                color: Colors.blue[100],
+              ),
+              Text(
+                "Upcoming Activities",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: screenHeight / 6,
+                child: UnfinishedEventList(),
+              ),
+              Text(
+                "Have you done this activity?",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: screenHeight / 6,
+                child: MaybeFinishedEventList(),
+              ),
+              Text(
+                "Completed Activities",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: screenHeight / 6,
+                child: CompletedEventList(),
               ),
             ],
           ),
-        ),
-      ),
-      body: ListView(
-        // Testing content below
-        children: [
-          Text("Home Screen"),
-
-          Center(
-            widthFactor: 0.2,
-            child: FutureBuilder(
-                future: _db.getUserProfilePic(),
-                builder: (context, snapshot) {
-                  return snapshot.hasData
-                      ? CircleAvatar(
-                    backgroundImage: NetworkImage(snapshot.data),
-                    backgroundColor: Colors.grey,
-                    radius: 50,
-                  )
-                      : Icon(Icons.person);
-                }),
+          drawer: NavBar(),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add, color: PresetColors.blue),
+            backgroundColor: Colors.blue[100],
+            onPressed: () {
+              if (currentDate.compareTo(DateTime.now()) < 0 &&
+                  !(today(currentDate))) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: const Text('Adding activities cannot be done for past dates!'),
+                  ),
+                );
+              } else {
+                print(currentDate.toString());
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => EventEditingPage(currentDate: currentDate)),
+                );
+              }
+            },
           ),
-        ],
-      ),
-      drawer: NavBar(),
+        );
+      }
     );
   }
 }
