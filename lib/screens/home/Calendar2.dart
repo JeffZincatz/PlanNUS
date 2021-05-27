3d20f4ea9cdf5fc046bf6466524d013bf412af10
Bernardus Krishna <krishnabernardus@gmail.com>
2021-05-27 23:06:40 +0800

    Added new features. Now events are indicated by small dots, and long press will show events of the day.

--- lib/screens/home/Calendar2.dart
+++ lib/screens/home/Calendar2.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:provider/provider.dart';
+import 'package:plannus/models/Event.dart';
+import 'package:plannus/models/EventDataSource.dart';
+import 'package:plannus/screens/home/TaskWidget.dart';
 
 class Calendar2 extends StatefulWidget {
+
   final Function updateCurrentDate;
 
   Calendar2({this.updateCurrentDate});
@@ -11,9 +16,14 @@ class Calendar2 extends StatefulWidget {
 }
 
 class _Calendar2State extends State<Calendar2> {
+
   @override
   Widget build(BuildContext context) {
+
+    List<Event> events = Provider.of<List<Event>>(context) ?? [];
+
     return SfCalendar(
+      dataSource: EventDataSource(events),
       headerStyle: CalendarHeaderStyle(
         textAlign: TextAlign.center,
         textStyle: TextStyle(
@@ -25,6 +35,14 @@ class _Calendar2State extends State<Calendar2> {
       initialSelectedDate: DateTime.now(),
       onTap: (CalendarTapDetails details) {
         widget.updateCurrentDate(details.date);
+      },
+      onLongPress: (CalendarLongPressDetails detail) {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return TaskWidget(date: detail.date, events: events);
+          },
+        );
       },
     );
   }
